1ec6c230c78a5582ee96b3ff889f2599e1511d63
cho45 <cho45@lowreal.net>
2010-02-15 12:38:34 +0900

    scripts

--- scripts/wifi_hook/setup.sh
+++ scripts/wifi_hook/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sudo cp ~/dotfiles/scripts/wifi_hook/wifi_hook.plist /System/Library/LaunchDaemons/
+sudo launchctl unload /System/Library/LaunchDaemons/wifi_hook.plist
 sudo launchctl load /System/Library/LaunchDaemons/wifi_hook.plist
 
