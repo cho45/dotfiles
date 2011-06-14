91fc59756ef21823b9932244f8681d1720332b12
cho45 <cho45@lowreal.net>
2011-06-14 15:47:23 +0900

    jslint は厳しすぎる。jshint に

--- .vim/vimparse.js
+++ .vim/vimparse.js
@@ -1,6 +1,25 @@
-#!/bin/sh
-# vim:set ft=sh:
-# npm install jslint
+#!/usr/bin/env node
 
-jslint --browser --no-node --no-es5 $1 | perl -na -e "s/ *[0-9]+ ([0-9]+),([0-9]+)/$1:\$1:\$2/ and print \$_"
+var JSHINT = require("./jshint.js").JSHINT;
+var fs = require("fs");
 
+var argv = process.argv;
+argv.shift();
+argv.shift();
+
+argv.forEach(function (filename) {
+	var source = fs.readFileSync(filename, 'utf-8');
+	var result = JSHINT(source, {
+		browser : true,
+		jquery  : true,
+		evil    : true,
+		passfail : false
+	});
+	if (!result) {
+		JSHINT.errors.forEach(function (error) {
+			if (!error) return;
+			if (error.evidence && error.evidence.length > 1000) return;
+			console.log([filename, error.line, error.character].join(':') + "\t" + error.reason);
+		});
+	}
+});
