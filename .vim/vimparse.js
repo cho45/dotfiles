18ebcce518bb86da351551397f4acfd870431161
cho45 <cho45@lowreal.net>
2011-06-20 10:52:58 +0900

    出さない警告を追加

--- .vim/vimparse.js
+++ .vim/vimparse.js
@@ -18,7 +18,18 @@ argv.forEach(function (filename) {
 	if (!result) {
 		JSHINT.errors.forEach(function (error) {
 			if (!error) return;
-			if (error.evidence && error.evidence.length > 1000) return;
+
+			// 可読性のために意図的にそうしているのでうざいし、そういう最適化は実行エンジンがすべきこと
+			if (error.reason.indexOf('is better written in dot notation') != -1) return;
+
+			if (error.evidence) {
+				// やたら長い行は圧縮されたJSコードとみなす
+				if (error.evidence.length > 1000) return;
+
+				// 閉じブレース前のセミコロンは省略可能に
+				if (error.reason.indexOf('Missing semicolon') != -1 && error.evidence.substring(error.character).match(/^\s*\}/)) return;
+			}
+
 			console.log([filename, error.line, error.character].join(':') + "\t" + error.reason);
 		});
 	}
