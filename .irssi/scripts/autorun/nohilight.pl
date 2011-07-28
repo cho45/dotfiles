d328a9b8f330422932d423d386bf66c5ba284cf1
cho45 <cho45@lowreal.net>
2011-07-28 22:06:52 +0900

    channel 名もふくむ

--- .irssi/scripts/autorun/nohilight.pl
+++ .irssi/scripts/autorun/nohilight.pl
@@ -50,7 +50,7 @@ Irssi::signal_add_first('print text', sub {
 	my ($dest, $text, $stripped) = @_;
 	return unless $dest->{level} & MSGLEVEL_HILIGHT;
 	my $regexp = join '|', @$nohilight;
-	if ($stripped =~ /$regexp/) {
+	if ($stripped =~ /$regexp/ || $dest->{target} =~ /$regexp/) {
 		$dest->{level} ^= MSGLEVEL_HILIGHT;
 		$dest->{level} |= MSGLEVEL_NOHILIGHT;
 		$dest->{window}->print($text, $dest->{level});
