a3ae5bed65719856b236a7290c40fe8c3c21c067
cho45 <cho45@lowreal.net>
2015-11-05 15:28:39 +0900

    update around vim

--- tools/setup_vim.sh
+++ tools/setup_vim.sh
@@ -3,10 +3,31 @@ set -e
 
 cd
 cd tmp
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2
-tar xjvf vim-7.4.tar.bz2
-cd vim74
-./configure --prefix=$HOME/app/vim --enable-multibyte --enable-gpm --enable-cscope --with-features=huge --enable-fontset --disable-gui --without-x --disable-xim --disable-perlinterp
+
+if [ -d vim ]; then
+	cd vim
+	git pull
+else
+	git clone --depth 1 https://github.com/vim/vim.git
+	cd vim
+fi
+
+cd src
+
+# make distclean
+
+./configure \
+	--prefix=$HOME/app/vim \
+	--enable-multibyte \
+	--enable-gpm \
+	--enable-cscope \
+	--with-features=huge \
+	--enable-fontset \
+	--disable-gui \
+	--without-x \
+	--disable-xim \
+	--disable-perlinterp
+
 make -j 2
 make install
 
